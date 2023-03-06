3c747ee9e530dd86202cc5f6f61a9190b99a0d1d
isinnur <isinngnyy@gmail.com>
2023-03-06 20:56:52 +0300

    game

---
+++ valorant_agents/src/App.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react'
+import './index.css';
+function App() {
+
+  const [agents, setAgents] = useState([]);
+
+  useEffect(() => {
+    fetch('https://valorant-api.com/v1/agents')
+      .then(response => response.json())
+      .then(data => {
+        console.log(data.data); // agent verilerini konsola yazdırıyoruz
+        setAgents(data.data);
+      })
+      .catch(error => console.log(error));
+  }, []);
+  return (
+    <div className="App">
+      {agents.map(agent => (
+        <div className='container' key={agent.uuid}>
+          <button className='touch'></button>
+          <h4 className='agents-name'>{agent.displayName}</h4>
+          <img className='image' src={agent.fullPortrait} alt={agent.displayName}></img>
+          <div className='infos'>
+            <span className='bio'>Biography: <br /> {agent.description}</span>
+            <span className='abilities'>Special Abilities: <br /> {agent.abilities.map(ability => ability.displayName).join(', ')}</span>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default App;
