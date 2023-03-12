492e8e08f339802b57e315380ce4cb8ac0c3f794
isinnur <isinngnyy@gmail.com>
2023-03-12 14:25:09 +0300

    add hexColor

---
+++ color-generator/src/App.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react'
+import SingleColor from './SingleColor'
+
+import Values from 'values.js'
+
+function App() {
+  const [color, setColor] = useState('');
+  const [error, setError] = useState(false);
+  const [list, setList] = useState(new Values('#f15025').all(10));
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    try {
+      let colors = new Values(color).all(10)
+      setList(colors)
+      console.log(colors);
+      setError(false)
+    } catch {
+      setError(true);
+      console.log(error);
+    }
+  }
+
+  return (
+    <>
+      <section className='container'>
+        <h3>color generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input type='text' value={color} onChange={(e) => setColor(e.target.value)} placeholder='#f15025' className={`${error ? 'error' : null}`} />
+          <button className='btn' type='submit'>Submit</button>
+        </form>
+      </section>
+
+      <section className='colors'>
+        {list.map((color, index) => {
+
+          return <SingleColor key={index} {...color} index={index} hexColor={color.hex} />
+        })}
+      </section>
+    </>
+  );
+}
+
+export default App
