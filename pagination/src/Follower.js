613222d22e4cd8c1b39748f81e9a110c677587e7
isinnur <isinngnyy@gmail.com>
2023-05-03 14:59:02 +0300

    pagination

---
+++ pagination/src/Follower.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Follower = ({ avatar_url, html_url, login }) => {
+  return <article className='card'>
+    <img src={avatar_url} alt={login}></img>
+    <h4>{login}</h4>
+    <a href={html_url} className='btn'>view profile</a>
+  </article>
+}
+
+export default Follower
---
+++ pagination/src/utils.js
@@ -0,0 +1,14 @@
+const paginate = (followers) => {
+    const itemsPerPage = 10;
+    const pages = Math.ceil(followers.length / itemsPerPage);
+
+
+    const newFollowers = Array.from({ length: pages }, (_, index) => {
+        const start = index * itemsPerPage;
+        return followers.slice(start, start + itemsPerPage)
+    });
+
+    return newFollowers;
+}
+
+export default paginate
